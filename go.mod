22ab11055cd9abb6b650470a539c6fbdb9c2dfc5
Tullio Sebastiani <tsebasti@redhat.com>
2024-06-14 14:41:32 +0200

    http2 example server no goroutine

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.22.3
 
 require golang.org/x/net v0.26.0
 
-require golang.org/x/text v0.16.0 // indirect
+require (
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
--- server.go
+++ server.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"sync"
+	"time"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
+
+var mu sync.Mutex
 
 func main() {
 	// Create a server on port 8000
 	// Exactly how you would run an HTTP/1.1 server
-	srv := &http.Server{Addr: ":8000", Handler: http.HandlerFunc(handle)}
+	http2Server := http2.Server{
+		MaxReadFrameSize: 512000,
+	}
+
+	srv := &http.Server{Addr: ":8000", Handler: h2c.NewHandler(http.HandlerFunc(handler), &http2Server)}
 
 	// Start the server with TLS, since we are running HTTP/2 it must be
 	// run with TLS.
@@ -17,9 +31,56 @@ func main() {
 	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	// Log the request protocol
-	log.Printf("Got connection over %s and content lenght was %d", r.Proto, r.ContentLength)
-	// Send a message back to the client
-	w.Write([]byte("Hello"))
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	count := 0
+	readStartTime := time.Now()
+	defer func() {
+		readEndTime := time.Now()
+		duration := readEndTime.Sub(readStartTime)
+		if duration.Seconds() > 10 {
+			fmt.Println(fmt.Sprintf("GGMGGM17 HandleStream read loop count %d time %s ts %d.%09d", count, duration.String(), readEndTime.Unix(), readEndTime.Nanosecond()))
+		}
+	}()
+
+	// TODO: minimize garbage, optimize recvBuffer code/ownership
+	const readSize = 8196
+	var bodyBuffer bytes.Buffer
+	for buf := make([]byte, readSize); ; {
+		count++
+		httpReadStart := time.Now()
+		n, err := r.Body.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println(bodyBuffer.Len())
+				return
+			} else {
+				panic(fmt.Errorf("error while reading body %s", err.Error()))
+			}
+		}
+		httpReadEnd := time.Now()
+		httpReadDuration := httpReadEnd.Sub(httpReadStart)
+		if httpReadDuration.Seconds() > 3 {
+			fmt.Println(fmt.Sprintf("GGMGGM18 HandleStream read loop single iter count %d time %s ts %d.%09d req obj %#v", count, httpReadDuration.String(), httpReadEnd.Unix(), httpReadEnd.Nanosecond(), r))
+		}
+		if n > 0 {
+			buf = buf[n:]
+			_, err := bodyBuffer.Write(buf)
+
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		allocBufStart := time.Now()
+		if len(buf) == 0 {
+			buf = make([]byte, readSize)
+		}
+		allocBufDuration := time.Now().Sub(allocBufStart)
+		if allocBufDuration.Seconds() > 3 {
+			fmt.Println(fmt.Sprintf("GGMGGM18 HandleStream read loop make buf single iter count %d time %s ts %d.%09d", count, httpReadDuration.String()))
+		}
+	}
+
 }
